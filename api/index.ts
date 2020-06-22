c252c7e59547c599ef17f73fa46fc4864697ca5f
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-23 03:45:18 +0900

    fix log

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,9 @@ import "@babel/plugin-transform-react-jsx";
 import convert from "reactel-to-html";
 
 export default (req: NowRequest, res: NowResponse) => {
+  console.log("req.body", req.body);
   const body = req.body;
   const { html } = body;
-  console.log("html", html);
   const convertedHtml = convert(html);
   res.status(200).json(convertedHtml);
 };
