192777a1ddd69bc83b6a70fd7001f6830a64e964
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-23 04:26:46 +0900

    con

---
+++ api/convert.ts
@@ -0,0 +1,9 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import convert from "reactel-to-html";
+
+export default (req: NowRequest, res: NowResponse) => {
+  const body = req.body;
+  const { html } = body;
+  const convertedHtml = convert(html);
+  res.status(200).json(convertedHtml);
+};
